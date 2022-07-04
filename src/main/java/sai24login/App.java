27ffcb8ddb05beb30e26ed40b3c6612bc25ce2ba
sai <smanapati1997@gmail.com>
2022-07-04 21:50:01 +0530

    asi developed maven programm

---
+++ src/main/java/sai24login/App.java
@@ -0,0 +1,13 @@
+package sai24login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
